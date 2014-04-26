dd9bc5225a3798b0da33bf05f89398865f632af9
Henrik Vendelbo <thepian@thepia.com>
2014-04-26 23:23:02 +0200

    enhance book in the app

--- _libs/product-app.js
+++ _libs/product-app.js
@@ -1,6 +1,28 @@
 require('./impl.js'); 
 require('./form.js');
 
+
+function enhance_book(el,role,config) {
+
+	var reader = require('../bower_components/book-reader/index.js');
+	var	book = new reader.Book(el,config);
+
+	return book;
+}
+
+function layout_book(el,layout,instance) {
+	if (instance) return instance.layout(layout);
+}
+
+function discard_book(el,role,instance) {
+	if (instance) instance.destroy(el);
+}
+
+Resolver("page").set("handlers.enhance.book", enhance_book);
+Resolver("page").set("handlers.layout.book", layout_book);
+Resolver("page").set("handlers.discard.book", discard_book);
+
+
 if (window.angular) {
 
     var productApp = angular.module('productApp', [  ]); // 'toggle-switch'
@@ -14,3 +36,4 @@ if (window.angular) {
     }]);
 
 }
+
