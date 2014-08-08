21e938c6d1f56404ea32a509eb5167eea72b52fc
Henrik Vendelbo <thepian@thepia.com>
2014-08-08 14:26:18 +0200

    known secret
    live in city

--- _includes/signup-config.js
+++ _includes/signup-config.js
@@ -4,51 +4,51 @@
 
     actions: {
       "new-beta-subscriber": {
-        mapName: { name:"cm-name", email:"cm-vjikj-vjikj", "from-country":"cm-fo-bjum", "native-language":"cm-f-bjutl" },
+        mapName: { name:"cm-name", email:"cm-vjikj-vjikj", "from-country":"cm-fo-bjum", "native-language":"cm-f-bjutl", "live-in":"cm-fo-cbky" },
         path: "/t/i/s/vjikj/"
       },
 
       "new-subscriber-en": {
-        mapName: { name:"cm-name", email:"cm-pdtdj-pdtdj", "from-country":"cm-fo-bjum", "native-language":"cm-f-bjutl" },
+        mapName: { name:"cm-name", email:"cm-pdtdj-pdtdj", "from-country":"cm-fo-bjum", "native-language":"cm-f-bjutl", "live-in":"cm-fo-cbky" },
         path: "/t/i/s/pdtdj/"
       },
       "new-subscriber-de": {
-        mapName: { name:"cm-name", email:"cm-pdtdh-pdtdh", "from-country":"cm-fo-bjum", "native-language":"cm-f-bjutl" },
+        mapName: { name:"cm-name", email:"cm-pdtdh-pdtdh", "from-country":"cm-fo-bjum", "native-language":"cm-f-bjutl", "live-in":"cm-fo-cbky" },
         path: "/t/i/s/pdtdh/"
 
       },
       "new-subscriber-ch": {
-        mapName: { name:"cm-name", email:"cm-pdthy-pdthy", "from-country":"cm-fo-bjum", "native-language":"cm-f-bjutl" },
+        mapName: { name:"cm-name", email:"cm-pdthy-pdthy", "from-country":"cm-fo-bjum", "native-language":"cm-f-bjutl", "live-in":"cm-fo-cbky" },
         path:   "/t/i/s/pdthy/"
 
       },
       "new-subscriber-cs": {
-        mapName: { name:"cm-name", email:"cm-pdthh-pdthh", "from-country":"cm-fo-bjum", "native-language":"cm-f-bjutl" },
+        mapName: { name:"cm-name", email:"cm-pdthh-pdthh", "from-country":"cm-fo-bjum", "native-language":"cm-f-bjutl", "live-in":"cm-fo-cbky" },
         path:   "/t/i/s/pdthh/"
 
       },
       "new-subscriber-pt": {
-        mapName: { name:"cm-name", email:"cm-pdthi-pdthi", "from-country":"cm-fo-bjum", "native-language":"cm-f-bjutl" },
+        mapName: { name:"cm-name", email:"cm-pdthi-pdthi", "from-country":"cm-fo-bjum", "native-language":"cm-f-bjutl", "live-in":"cm-fo-cbky" },
         path:   "/t/i/s/pdthi/"
 
       },
       "new-subscriber-fr": {
-        mapName: { name:"cm-name", email:"cm-pdthl-pdthl", "from-country":"cm-fo-bjum", "native-language":"cm-f-bjutl" },
+        mapName: { name:"cm-name", email:"cm-pdthl-pdthl", "from-country":"cm-fo-bjum", "native-language":"cm-f-bjutl", "live-in":"cm-fo-cbky" },
         path:   "/t/i/s/pdthl/"
 
       },
       "new-subscriber-it": {
-        mapName: { name:"cm-name", email:"cm-pdthr-pdthr", "from-country":"cm-fo-bjum", "native-language":"cm-f-bjutl" },
+        mapName: { name:"cm-name", email:"cm-pdthr-pdthr", "from-country":"cm-fo-bjum", "native-language":"cm-f-bjutl", "live-in":"cm-fo-cbky" },
         path:   "/t/i/s/pdthr/"
 
       },
       "new-subscriber-jp": {
-        mapName: { name:"cm-name", email:"cm-pdthk-pdthk", "from-country":"cm-fo-bjum", "native-language":"cm-f-bjutl" },
+        mapName: { name:"cm-name", email:"cm-pdthk-pdthk", "from-country":"cm-fo-bjum", "native-language":"cm-f-bjutl", "live-in":"cm-fo-cbky" },
         path:   "/t/i/s/pdthk/"
 
       },
       "new-subscriber-pl": {
-        mapName: { name:"cm-name", email:"cm-pdthd-pdthd", "from-country":"cm-fo-bjum", "native-language":"cm-f-bjutl" },
+        mapName: { name:"cm-name", email:"cm-pdthd-pdthd", "from-country":"cm-fo-bjum", "native-language":"cm-f-bjutl", "live-in":"cm-fo-cbky" },
         path:   "/t/i/s/pdthd/"
 
       }
