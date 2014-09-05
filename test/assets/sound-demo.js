5edb58d47dd5baa34edf473ea50f392aac8bf6b1
Henrik Vendelbo <thepian@thepia.com>
2014-09-05 19:49:54 +0200

    more sound test buttons

--- test/assets/sound-demo.js
+++ test/assets/sound-demo.js
@@ -11,4 +11,27 @@ testCmd.addSpoken("two", {
 	ogg: "two.ogg"
 }, "main");
 
+
+testCmd.addSpoken("one", {
+	mp3: "one.mp3",
+	ogg: "one.ogg"
+}, "a");
+
+testCmd.addSpoken("two", {
+	mp3: "two.mp3",
+	ogg: "two.ogg"
+}, "a");
+
+
+testCmd.addSpoken("one", {
+	mp3: "one.mp3",
+	ogg: "one.ogg"
+}, "b");
+
+testCmd.addSpoken("two", {
+	mp3: "two.mp3",
+	ogg: "two.ogg"
+}, "b");
+
+
 }();
