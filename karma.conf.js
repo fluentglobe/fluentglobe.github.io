01da189082294cbbef20ef776b180b07f59ebd4b
Henrik Vendelbo <thepian@thepia.com>
2014-10-16 08:28:20 +0200

    firefox

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,6 @@
 module.exports = function(config) {
 	config.set({
-		browsers: ['Chrome'],
+		browsers: ['Chrome','Firefox'],
 		frameworks: ['jasmine'],
 		files: [
 			'client/code/**/hello*.js',
