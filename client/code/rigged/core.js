adfc2daf700ebd0e92ce75b2b562f0af99bd12d1
Henrik Vendelbo <thepian@thepia.com>
2014-10-30 08:01:44 +0100

    router.js

--- client/code/rigged/core.js
+++ client/code/rigged/core.js
@@ -3,6 +3,9 @@
 //= ../../components/es5-shim/es5-shim.js
 //= ../../components/essentialjs/essential.js
 //= ../../../lib/client/browserify.js
+!function() {
+//= ../../../lib/client/router.js
+}();
 
 !function(globals,document) {
 
